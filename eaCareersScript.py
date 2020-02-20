39360ca9ee818334f10181f3b2c5cc33859ead5b
Nathan <nathanialhenry92@gmail.com>
2020-02-19 20:46:25 -0800

    changed saving variables as string to JSON objects, and used dictionaries for comparison, added TODO

--- eaCareersScript.py
+++ eaCareersScript.py
@@ -1,7 +1,9 @@
-from selenium import webdriver
-import os, argparse, smtplib
+# TODO find object best for comparison of the two sets of Listings and format the email better
 
-# TODO improve email formatting 
+from selenium import webdriver
+import os, argparse, smtplib, json
+
+# TODO improve email formatting
 # create command line argumets for sensitive/desired information
 parser = argparse.ArgumentParser()
 parser.add_argument("--path", type=str, default="/usr/bin/chromedriver", help="Insert Filepath for selenium web driver (chromedriver, gecko, etc)" )
@@ -60,15 +62,16 @@ jobListing = job_table.find_elements_by_class_name('eacom-jobs-list__row')
 # declare jobs of interest and variables to store results
 JOBS_OF_INTEREST = args.job
 
-results_dict = []
+results_dict = {}
+results_dict['Listings'] = []
 # Loop through listings from the web element and create dicts/lists
 for listing in jobListing:
   for job in JOBS_OF_INTEREST:
     if (job in listing.text):
-        result_content = {
-            (listing.get_attribute("href")) : (listing.text.split("\n")[0])
-            }
-        results_dict.append(result_content)
+        results_dict['Listings'].append( {
+        'url' : (listing.get_attribute("href")),
+        'job' : (listing.text.split("\n")[0])
+        })
 
 # creates jobFile.txt if it doesn't already exist
 file_check = os.path.isfile('jobFile.txt')
@@ -77,52 +80,41 @@ if file_check == False:
         f.write('')
     f.close()
 
-# writes new listings to a txt file. This isn't the original way I wanted to compare older listings to new listings, but I couldn't get the syntax to match consistently
-# between a variable within the script and one created from reading lines in a .txt
-with open('newJobFile.txt','w') as newJobFile:
-    for i in results_dict:
-        newJobFile.write(str(i))
-newJobFile.close()
+# open previously saved results from JSON and append them to older_results variable for comparison
+older_results_dict = {}
+older_results_dict['Listings'] = []
+try:
+    with open('jobFile.txt', 'r') as json_file:
+        stored_listing = json.load(json_file)
+        older_results_dict = dict(stored_listing)
+    file.close()
+except Exception as e:
+    print("There has been an Exception at %s" % (e))
 
-# open previously saved results from jobFile.txt and append them to older_results variable for comparison
-older_results = []
-with open('jobFile.txt', 'r') as jobFile:
-    for i in jobFile:
-        older_results.append(i)
-jobFile.close()
-
-# creates variable from reading the .txt written by the new listing results variable
-results_comparable = []
-with open('newJobFile.txt', 'r') as newJobFile:
-    for i in newJobFile:
-        results_comparable.append(i)
-newJobFile.close()
 
 # compare new listing results vs older listing results and append the difference to variables
-older_results_to_write = []
-results_to_email= []
-for i in results_comparable:
-    if i not in older_results:
-        older_results_to_write.append(i)
-        results_to_email.append(i)
-        results_to_email.append("\n")
-print("these results were found to be recent additions to the ea site:{0}".format(results_to_email))
+
+new = set(results_dict['Listings'])
+old = set(older_results_dict['Listings'])
+
+results_difference = new - old
+
+results_to_email = dict(results_difference)
+
+print("these results were found to be recent additions to the ea site:{0}  =  {1}".format(results_to_email.get('job'), results_to_email.get('url')))
 
 # update the jobFile.txt document with most up to date job listings
-with open('jobFile.txt', 'w') as z:
-    for i in older_results:
-        z.write(i)
-    for i in older_results_to_write:
-        z.write(str(i))
-z.close()
+with open('jobFile.txt', 'w') as file:
+    json.dump(results_dict, file)
+file.close()
 
 # send email with new listings if there was any changes between older scrapes and newer scrapes
 if results_to_email:
     print("sending email...")
-    text = ("Hello, \n These are the new job listings found on the EA Vancouver Careers website: \n %s" %(results_to_email))
+    text = ("Hello, \n These are the new job listings found on the EA Vancouver Careers website: \n %s" %(results_to_email.get('job'), results_to_email.get('url')))
     send_email(text)
 else:
     print("No new listings to report")
-
+#
 # close webdriver
 driver.quit()
